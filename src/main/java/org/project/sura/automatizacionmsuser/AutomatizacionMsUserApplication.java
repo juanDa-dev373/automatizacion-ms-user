2ac136adf2c1a838b3700f35514e095c790f28a9
juanDa-dev373 <juand.riosf1@uqvirtual.edu.co>
2025-10-16 18:39:51 -0500

    first commit

---
+++ src/main/java/org/project/sura/automatizacionmsuser/AutomatizacionMsUserApplication.java
@@ -0,0 +1,13 @@
+package org.project.sura.automatizacionmsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizacionMsUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatizacionMsUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/project/sura/automatizacionmsuser/AutomatizacionMsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.sura.automatizacionmsuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatizacionMsUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
